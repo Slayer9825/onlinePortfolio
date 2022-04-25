19fb8f57660d33f440659dc8ff51613075617e8d
Your Name <iamjanustheman@gmail.com>
2022-04-25 21:04:41 +0100

    Added color change in light mode for links, further improved media query responsiveness and styling.

--- script/script.js
+++ script/script.js
@@ -2,9 +2,16 @@ function darkModeSwap() {
     let bodyVar = document.body;
     let buttonDarkTheme = document.getElementById("darkModeButtonToggle")
     
+    const anchorLinkElement = document.getElementById('contactLinkName')
+    anchorLinkElement.classList.toggle("linkColorSwap")
+
+    const anchorLinkElement2 = document.getElementById('contactLinkName2')
+    anchorLinkElement2.classList.toggle("linkColorSwap")
+    
+
+
     bodyVar.classList.toggle("darkModeSwap")
     buttonDarkTheme.classList.toggle("darkLightModeButtonToggle")
-
 
 }
 
