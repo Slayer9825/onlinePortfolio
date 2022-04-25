59d0a66cbeac8307d8f1d61972eb5d2808c7156d
Your Name <iamjanustheman@gmail.com>
2022-04-25 05:11:23 +0100

    Added structure, styling and content for Contact page. Further tuned media queries

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-
 function darkModeSwap() {
-    let bodyVar = document.getElementsByTagName="body"
+    let bodyVar = document.body;
+    let buttonDarkTheme = document.getElementById("darkModeButtonToggle")
+    
+    bodyVar.classList.toggle("darkModeSwap")
+    buttonDarkTheme.classList.toggle("darkLightModeButtonToggle")
+
+
 }
+
+let darkModeButton = document.getElementById("darkModeButtonToggle")
+darkModeButton.addEventListener("click", darkModeSwap)
